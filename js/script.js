e7b365e44557c2bb95c45aee34dac300737c6642
Nikita <nikita1.citrusbug@gmail.com>
2022-04-21 17:36:59 +0530

    changes saved

--- js/script.js
+++ js/script.js
@@ -1,41 +1,46 @@
 
-    function seterror(id,error){
-        element = document.getElementById(id);
-        element.getElementsByClassName('formerror')[0].innerHTML = error;
+function seterror(id, error) {
+    element = document.getElementById(id);
+    element.getElementsByClassName('formerror')[0].innerHTML = error;
 
+}
+function validateForm() {
+    var returnval = true;
+    var pname = document.forms['productForm']['fname'].value;
+    if (pname.length < 5) {
+        alert("minimum length required is 6");
+        returnval = false;
     }
-    function validateForm(){
-        var returnval = true;
-        var pname = document.forms['productForm']['fname'].value;
-        if(pname.length<5){
-            seterror("pname","**length is too short");
-            returnval= false;
-        }
-        var disc = document.forms['productForm']['fdisc'].value;
-    
-        if(disc.length<2){
-            seterror("disc","**length is too short");
-            returnval= false;
-        }
-        if(disc.length>250){
-            seterror("disc","**length is too long");
-            returnval= false;
-        }
-        var productprice = document.forms['productForm']['fproductprice'].value;
-        if(Number(productprice)>2000){
-            alert("price is not greater than 2000");
-            returnval= false;
-        }
-        
-        var dis = document.forms['productForm']['fdis'].value;
-        if(Number(dis)>=100){
-            alert("discount is not valid more than 100");
-            returnval= false;
-        }
-        
-        
-        return returnval;
-
+    var disc = document.forms['productForm']['fdisc'].value;
+    if (disc.length = 0) {
+        seterror("disc", "**please enter the discription");
+        returnval = false;
+    }
+    if (disc.length < 2) {
+        alert("minimum length required is 3");
+        returnval = false;
+    }
+    if (disc.length > 250) {
+        alert("maximum length is 250");
+        returnval = false;
+    }
+    var productprice = document.forms['productForm']['fproductprice'].value;
+    if (Number(productprice) < 0) {
+        alert("price is not valid in minus");
+        returnval = false;
+    }
+    var dis = document.forms['productForm']['fdis'].value;
+    if (Number(dis) < 0) {
+        alert("discount is not valid in minus");
+        returnval = false;
+    }
+    if (Number(dis) > 100) {
+        alert("discount is not valid more than 100");
+        returnval = false;
     }
 
-    
+
+    return returnval;
+
+}
+
